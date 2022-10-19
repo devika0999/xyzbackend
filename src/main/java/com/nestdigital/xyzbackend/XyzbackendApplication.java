c092d801f9a899dccb6cbde06ceb9f01de3d8c39
devika0999 <krishnadevika445@gmail.com>
2022-10-19 08:52:15 +0530

    xyz backend

---
+++ src/main/java/com/nestdigital/xyzbackend/XyzbackendApplication.java
@@ -0,0 +1,13 @@
+package com.nestdigital.xyzbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XyzbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XyzbackendApplication.class, args);
+	}
+
+}
